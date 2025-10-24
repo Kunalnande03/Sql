142931db83c42adb5a66641f90e0ffed2813a856
Kunal nande <nandekunal03@gmail.com>
2025-10-24 08:53:37 +0530

    Add files via upload

---
+++ normal_command.sql
@@ -0,0 +1,10 @@
+create database hospital;
+use hospital;
+create table doctor(doc_id int(3),doc_name varchar(20),Specialisation varchar(25));
+insert into doctor(doc_id,doc_name,Specialisation) values(1,"Vibhuti ","Cardiologeist");
+insert into doctor(doc_id,doc_name,Specialisation) values(2,"Narayn ","Cardiologeist");
+insert into doctor(doc_id,doc_name,Specialisation) values(3,"Sohan ","Neurologeist");
+select * from doctor;
+update doctor set specialisation="General Doctor";
+delete * from doctor;
+commit;
